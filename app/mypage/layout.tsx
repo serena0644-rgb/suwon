0e7c517cc31446063d066cdd3d09d40265fd6612
serena0644-rgb <serena0644@gmail.com>
2026-09-15 17:49:05 +0900

    Use explicit ReactNode type in mypage layout

--- app/mypage/layout.tsx
+++ app/mypage/layout.tsx
@@ -1,5 +1,6 @@
+import type { ReactNode } from "react";
 import "./mypage.css";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+export default function Layout({ children }: { children: ReactNode }) {
   return children;
 }
